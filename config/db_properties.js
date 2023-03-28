0fecdb6a2117643cc394a75d4a8efa78c5f20a60
Omkar Shinde <omkarshinde5426@gmail.com>
2023-03-29 02:06:47 +0530

    changed db

--- config/db_properties.js
+++ config/db_properties.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host:"digihal-db.cydpvtg1mc9v.ap-south-1.rds.amazonaws.com",
-  user:"admin",
-  password:"Omega7809",
-  dbName:"digihal",
+  host:"sql12.freemysqlhosting.net",
+  user:"sql12609326",
+  password:"2QDYae4z5v",
+  dbName:"sql12609326",
     dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +11,30 @@ module.exports = {
     idle: 10000
   }
 };
+// module.exports = {
+//   host:"digihal-db.cydpvtg1mc9v.ap-south-1.rds.amazonaws.com",
+//   user:"admin",
+//   password:"Omega7809",
+//   dbName:"digihal",
+//     dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+// module.exports = {
+//   host:"127.0.0.1",
+//   port:"3306",
+//   user:"root",
+//   password:"Omega@7809",
+//   dbName:"digihal",
+//     dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
